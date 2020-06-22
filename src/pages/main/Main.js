aa5bdd373386d7672178309d27538ad32e8e053e
mokyoungg <mokyoungg@gmail.com>
2020-06-22 11:09:54 +0900

    초기세팅
    Routes
    
    style-component
    
    prettier

---
+++ src/pages/main/Main.js
@@ -0,0 +1,16 @@
+import React, { Component, useState, useEffect } from "react";
+import { Link, withRouter } from "react-router-dom";
+import styled from "styled-components";
+
+const Main = () => {
+  return <Wrap>Hello World!</Wrap>;
+};
+
+export default Main;
+
+//style
+
+const Wrap = styled.div`
+  background: yellowgreen;
+  font-size: 150px;
+`;
